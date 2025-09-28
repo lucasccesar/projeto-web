28b7f0a2167391276fecaabc785294457687be6e
PedroViniciusMD <pedrov.delgado.dev@gmail.com>
2025-09-28 18:05:10 -0300

    feat: implement BookClub service with create, update, delete, and find methods

---
+++ backend/src/main/java/br/com/bookly/services/BookClubService.java
@@ -0,0 +1,19 @@
+package br.com.bookly.services;
+
+import br.com.bookly.entities.BookClub;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+import java.util.List;
+import java.util.UUID;
+
+public interface BookClubService {
+
+    public BookClub createBookClub(BookClub bookClub);
+    public boolean deleteBookClub(BookClub bookClub);
+    public boolean deleteBookClubById(UUID id);
+    public BookClub updateBookClub(BookClub bookClub);
+    public BookClub findBookClubById(UUID id);
+    public BookClub findBookClubByName(String name);
+    public Page<BookClub> listBookClub(Pageable pageable);
+}
---
+++ backend/src/main/java/br/com/bookly/services/impl/BookClubServiceImpl.java
@@ -0,0 +1,120 @@
+package br.com.bookly.services.impl;
+
+import br.com.bookly.entities.BookClub;
+import br.com.bookly.repositories.BookClubRepository;
+import br.com.bookly.services.BookClubService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+
+
+import java.util.UUID;
+
+@Service
+public class BookClubServiceImpl implements BookClubService {
+
+    @Autowired
+    BookClubRepository bookClubRepository;
+
+    @Override
+    public BookClub createBookClub(BookClub bookClub) {
+
+        if (bookClub.getName() == null || bookClub.getName().isBlank()) {
+            return null;
+        }
+
+        if (bookClub.getTheme() == null || bookClub.getTheme().isBlank()) {
+            return null;
+        }
+
+        // descrição não obrigatoria seta como vazio
+        if (bookClub.getDescription() == null || bookClub.getDescription().isBlank()) {
+            bookClub.setDescription("");
+        }
+
+        // verifica se já tem um clube com o mesmo nome
+        if (bookClubRepository.existsByNameIgnoreCase(bookClub.getName())) {
+            return null;
+        }
+
+        return bookClubRepository.save(bookClub);
+    }
+
+    @Override
+    public BookClub updateBookClub(BookClub bookClub) {
+
+
+        //veridica se o id do clube foi informado para atualizar
+        if (bookClub.getIdBookClub() == null) {
+            return null;
+        }
+
+
+        BookClub exists = bookClubRepository.findById(bookClub.getIdBookClub()).orElse(null); // se o clube não existir retorna null
+        if (exists == null) {
+            return null;
+        }
+
+
+        if (bookClub.getName() == null || bookClub.getName().isBlank()) {
+            return null;
+        }
+
+
+        if (bookClub.getTheme() == null || bookClub.getTheme().isBlank()) {
+            return null;
+        }
+
+        // Se o nome foi alterado, verifica duplicação
+        if (!exists.getName().equals(bookClub.getName()) // verifica se foi feita alteração no nome
+                && bookClubRepository.existsByNameIgnoreCase(bookClub.getName()))// verifica se existe algum clube com esse nome
+            {return null;}
+
+
+        exists.setName(bookClub.getName());
+        exists.setTheme(bookClub.getTheme());
+        exists.setDescription(bookClub.getDescription());
+
+        return bookClubRepository.save(exists);
+    }
+
+    @Override
+    public boolean deleteBookClub(BookClub bookClub) {
+
+        BookClub exists = bookClubRepository.findById(bookClub.getIdBookClub()).orElse(null);
+
+        if (exists == null) {
+            return false;
+        }
+
+        bookClubRepository.delete(exists);
+        return true;
+    }
+
+    @Override
+    public boolean deleteBookClubById(UUID id) {
+
+        if (bookClubRepository.existsById(id) == false) {
+            return false;
+        }
+
+        bookClubRepository.deleteById(id);
+        return true;
+    }
+
+    @Override
+    public BookClub findBookClubById(UUID id) {
+        return bookClubRepository.findById(id).orElse(null);
+    }
+
+    @Override
+    public BookClub findBookClubByName(String name) {
+        return bookClubRepository.findBookClubByNameIgnoreCase(name);
+    }
+
+    @Override
+    public Page<BookClub> listBookClub(Pageable pageable) {
+        return bookClubRepository.findAll(pageable);
+    }
+}
