ccf7e31fab5eb7cf39a663b0f6cfd72f012da581
Lucas Cassiano <lucasccesar05@gmail.com>
2025-11-25 01:11:52 -0300

    feat: add account and edit account pages

---
+++ frontend/js/account.js
@@ -0,0 +1,38 @@
+const token = localStorage.getItem('token');
+
+async function getUser() {
+    const res = await fetch('http://localhost:8080/api/users/me', {
+        method: 'GET',
+        headers: { Authorization: 'Bearer ' + token }
+    });
+    return await res.json();
+}
+
+async function getStatusBooks(id) {
+    const res = await fetch(`http://localhost:8080/api/readingstatus/idUser/${id}`, {
+        method: 'GET',
+        headers: { Authorization: 'Bearer ' + token }
+    });
+    const data = await res.json();
+    return data.content || [];
+}
+
+const user = await getUser();
+
+document.getElementById('userName').textContent = user.name;
+document.getElementById('userEmail').textContent = user.email;
+document.getElementById('userType').textContent = user.type == 'ADMINISTRATOR' ? 'Administrador' : 'Cliente';
+
+document.getElementById('editAccountBtn').addEventListener('click', () => {
+    window.location.href = './editAccount.html';
+});
+
+const statusList = await getStatusBooks(user.id);
+
+const readingNow = statusList.filter(s => s.status === 'LENDO').length;
+const readFinished = statusList.filter(s => s.status === 'LIDO').length;
+const wantToRead = statusList.filter(s => s.status === 'QUERO_LER').length;
+
+document.getElementById('readingNow').textContent = `Lendo agora (${readingNow})`;
+document.getElementById('readFinished').textContent = `Já li (${readFinished})`;
+document.getElementById('wantToRead').textContent = `Quero ler (${wantToRead})`;
---
+++ frontend/js/editAccount.js
@@ -0,0 +1,64 @@
+const token = localStorage.getItem('token');
+
+async function getUser() {
+    const res = await fetch('http://localhost:8080/api/users/me', {
+        headers: { Authorization: 'Bearer ' + token },
+    });
+    return await res.json();
+}
+
+const user = await getUser();
+
+const form = document.getElementById('editAccountForm');
+const nameInput = document.getElementById('name');
+const emailInput = document.getElementById('inputEmail');
+const passwordInput = document.getElementById('inputPassword');
+const typeSelect = document.getElementById('inputType');
+
+nameInput.value = user.name;
+emailInput.value = user.email;
+typeSelect.value = user.type;
+
+
+form.addEventListener('submit', async (e) => {
+    e.preventDefault();
+
+    const name = nameInput.value.trim();
+    const email = emailInput.value.trim();
+    const password = passwordInput.value.trim();
+    const userType = typeSelect.value;
+    console.log(userType)
+
+    if (!name || !email || !password) {
+        alert('Por favor, preencha todos os campos.');
+        return;
+    }
+
+    try {
+        const response = await fetch(`http://localhost:8080/api/users/${user.id}`, {
+            method: 'PUT',
+            headers: {
+                'Content-Type': 'application/json',
+                Authorization: 'Bearer ' + token
+            },
+            body: JSON.stringify({
+                name,
+                email,
+                password,
+                type: userType
+            }),
+        });
+
+        if (!response.ok) {
+            alert('Erro ao atualizar conta. Verifique os dados.');
+            return;
+        }
+
+        alert('Dados atualizados com sucesso!');
+        window.location.href = './account.html';
+
+    } catch (err) {
+        console.error(err);
+        alert('Erro ao conectar com o servidor.');
+    }
+});
